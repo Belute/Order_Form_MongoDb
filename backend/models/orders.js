b776df43b9e3a07b28667f9caac1f7f107c5d2d7
Justinas Bagdonas <JUBAGDONAS@GMAIL.COM>
2022-04-13 23:16:51 +0300

    Order_Mongo

---
+++ backend/models/orders.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const ordersSchema = new mongoose.Schema({
+    id: Number,
+    first_name: String,
+    last_name: String,
+    address: String,
+    city: String,
+    post_code: String,
+    email: String,
+    phone: String,
+    shipping_method: String,
+    payment_method: String,
+    product: String,
+    total: Number
+})
+
+export default mongoose.model('Orders', ordersSchema, 'Orders')
